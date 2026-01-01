87d24b62024d78e8ce6a5b3ef7d35e6b06fbc04e
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-01-01 16:01:01 +0900

    fix: マジックナンバー、グリッドサイズ、スケール、最大輝度を定数化、所持しているpixel_dataを配列で管理

--- src/main.rs
+++ src/main.rs
@@ -4,62 +4,77 @@ use std::io::Error;
 use std::io::Write;
 use std::io::{self, ErrorKind};
 
+const MAGIC_NUMBER: &str = "P3"; // マジックナンバー
+const GRID_SIZE: usize = 5; // 5 * 5のグリッド
+const SCALE: usize = 50; // 1マス50ピクセル
+const MAX_BRIGHT: usize = 255; // 最大輝度
+
 #[derive(Debug)]
 struct FileStructure {
-    pub magic_number: &'static str,
-    height: usize,
-    width: usize,
-    max_bright: usize,
-    pixel_data: Vec<Vec<(u8, u8, u8)>>,
+    pixel_data: [[(u8, u8, u8); GRID_SIZE]; GRID_SIZE],
 }
 
 impl FileStructure {
-    fn new(
-        height: usize,
-        width: usize,
-        max_bright: usize,
-        pixel_data: Vec<Vec<(u8, u8, u8)>>,
-    ) -> Self {
-        FileStructure {
-            magic_number: "P3",
-            height,
-            width,
-            max_bright,
-            pixel_data,
-        }
+    fn new(pixel_data: [[(u8, u8, u8); GRID_SIZE]; GRID_SIZE]) -> Self {
+        FileStructure { pixel_data }
     }
 
-    fn height(&self) -> usize {
-        self.height
+    fn set_pixel_data(&mut self, pixel_data: [[(u8, u8, u8); GRID_SIZE]; GRID_SIZE]) {
+        self.pixel_data = pixel_data;
     }
 
-    fn width(&self) -> usize {
-        self.width
-    }
-
-    fn max_bright(&self) -> usize {
-        self.max_bright
-    }
-
-    fn pixel_data(&self) -> &Vec<Vec<(u8, u8, u8)>> {
+    fn pixel_data(&self) -> &[[(u8, u8, u8); GRID_SIZE]; GRID_SIZE] {
         &self.pixel_data
     }
 
+    fn change_grid(&mut self, w: usize, h: usize, color: (u8, u8, u8)) {
+        self.pixel_data[h][w] = color;
+    }
+
     fn create_ppm(&self, file: &mut File) -> io::Result<()> {
-        let (width, height) = (self.width(), self.height());
-        writeln!(file, "{}", self.magic_number)?;
-        writeln!(file, "{} {}", width, height)?;
-        writeln!(file, "{}", self.max_bright())?;
+        let physical_width = GRID_SIZE * SCALE;
+        let physical_height = GRID_SIZE * SCALE;
+        writeln!(file, "{}", MAGIC_NUMBER)?;
+        writeln!(file, "{} {}", physical_width, physical_height)?;
+        writeln!(file, "{}", MAX_BRIGHT)?;
 
         let pixel_data = self.pixel_data();
-        for pixel in pixel_data {
-            for &data in pixel.iter() {
-                write!(file, "{} {} {}  ", data.0, data.1, data.2)?;
+        let mut image = [[(0, 0, 0); GRID_SIZE * SCALE]; GRID_SIZE * SCALE];
+
+        // h: pixel_dataのインデックス
+        // w: pixel_data[i]のインデックス
+        // pixel_data[h][w]で(h, w)グリッド
+        for (h, pixel) in pixel_data.iter().enumerate() {
+            for (w, &p) in pixel.iter().enumerate() {
+                for hi in image.iter_mut().skip(h * SCALE).take(SCALE) {
+                    for hij in hi.iter_mut().skip(w * SCALE).take(SCALE) {
+                        *hij = p;
+                    }
+                }
+            }
+        }
+
+        for i in image {
+            for p in i {
+                let (r, g, b) = (p.0, p.1, p.2);
+                write!(file, "{} {} {}  ", r, g, b)?;
             }
             writeln!(file)?;
         }
 
         Ok(())
+    }
+
+    #[allow(unused)]
+    fn make_black(&mut self) {
+        let black_pixel = [[(0, 0, 0); GRID_SIZE]; GRID_SIZE];
+        self.set_pixel_data(black_pixel);
+    }
+
+    #[allow(unused)]
+    fn make_white(&mut self) {
+        let white_pixel = [[(255, 255, 255); GRID_SIZE]; GRID_SIZE];
+        self.set_pixel_data(white_pixel);
     }
 }
 
@@ -97,13 +112,17 @@ fn main() -> io::Result<()> {
             return Err(Error::new(ErrorKind::InvalidInput, "invalid to argumetns"));
         };
 
-        let height = 2;
-        let width = 2;
-        let max_bright = 255;
-        // サンプルデータ
-        let pixel_data = vec![vec![(255, 0, 0), (0, 0, 0)], vec![(0, 0, 0), (0, 0, 255)]];
-        let fs = FileStructure::new(height, width, max_bright, pixel_data);
-        println!("FileStructure: {:#?}", fs);
+        let pixel_data = [[(0, 0, 0); GRID_SIZE]; GRID_SIZE];
+
+        let mut fs = FileStructure::new(pixel_data);
+        fs.make_white();
+
+        let red = (255, 0, 0);
+        let blue = (0, 0, 255);
+        let green = (0, 255, 0);
+        fs.change_grid(2, 2, green);
+        fs.change_grid(0, 0, red);
+        fs.change_grid(4, 4, blue);
 
         fs.create_ppm(&mut file)?;
 
